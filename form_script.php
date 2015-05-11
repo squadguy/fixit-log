fe0ac04acdeb30bc802548977282b501fa946b88
squadguy <programmingfire@gmail.com>
2015-05-11 11:37:24 -0500

    Input HTML and php for first page of job sheet.

---
+++ form_script.php
@@ -0,0 +1,19 @@
+<?php
+	$case=$_POST['case']
+	$owner_first_name=$_POST['owner_first_name'];
+	$owner_last_name=$_POST['owner_last_name'];
+	$phone=$_POST['phone'];
+	$email=$_POST['email'];
+	$os=$_POST['os'];
+	$sn=$_POST['sn'];
+	$ram=$_POST['ram'];
+	$cpu=$_POST['cpu'];
+	$equipment=$_POST['equipment'];
+	$co=$_POST['co'];
+	$began=$_POST['began'];
+	$previous_work=$_POST['previous_work'];
+	$comments=$_POST['comments']
+
+	print "<p>$case $owner_first_name $owner_last_name $phone $email $os $sn $ram $cpu $equipment $co
+		$began $previous_work $comments</p>";
+?>
