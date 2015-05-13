b79099b75e4133c20464eacae9a856ec17b60935
squadguy <cwampnar@gmail.com>
2015-05-12 19:58:49 -0500

    Complete HTML and php variables.

--- form_script.php
+++ form_script.php
@@ -13,7 +13,12 @@
 	$began=$_POST['began'];
 	$previous_work=$_POST['previous_work'];
 	$comments=$_POST['comments'];
-
+	$boot_time_intial=$_POST['boot_time_intial'];
+	$memory_use_intial=$_POST['memory_use_intial'];
+	$evaluation=$_POST['evaluation'];
+	$actions=$_POST['actions'];
+	$recommendations=$_POST['recommendations'];
+	
 	print "<p>$case $owner_first_name $owner_last_name $phone $email $os $sn $ram $cpu $equipment $co
 		$began $previous_work $comments</p>";
 ?>
