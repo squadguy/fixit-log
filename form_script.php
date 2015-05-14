25a61032f7895796cbd4031fe61cf42d01854130
squadguy <programmingfire@gmail.com>
2015-05-13 21:46:44 -0500

    Change order of PHP variables

--- form_script.php
+++ form_script.php
@@ -16,10 +16,10 @@ if (!$db_selected) {
 	die('Can\'t use ' . DB_NAME . ': ' . mysql_error());
 }
 
-echo 'connected sucesfully';
+/*echo 'connected sucesfully';*/
 
 $case_id=$_POST['case_id'];
-$date=$_POST['date'];
+$day=$_POST['day'];
 $owner_first_name=$_POST['owner_first_name'];
 $owner_last_name=$_POST['owner_last_name'];
 $phone=$_POST['phone'];
@@ -32,27 +32,30 @@ $os_make=$_POST['os_make'];
 $os_type=$_POST['os_type'];
 $os_architecture=$_POST['os_architecture'];
 $sn=$_POST['sn'];
-$ram=$_POST['ram'];
-$cpu_type=$_POST['cpu_type'];
+$ram_amount=$_POST['ram_amount'];
+$cpu_make=$_POST['cpu_make'];
+$cpu_model=$_POST['cpu_model'];
 $cpu_architecture=$_POST['cpu_architecture'];
-$equipment=$_POST['equipment'];
+$equipment_other=$_POST['equipment_other'];
 $co=$_POST['co'];
 $began=$_POST['began'];
 $previous_work=$_POST['previous_work'];
 $previous_who=$_POST['previous_who'];
-$comments=$_POST['comments'];
+$previous_when=$_POST['previous_when'];
+$comments_initial=$_POST['comments_initial'];
 $boot_time_initial=$_POST['boot_time_initial'];
 $memory_use_initial=$_POST['memory_use_initial'];
 $cpu_use_initial=$_POST['cpu_use_initial'];
 $evaluation=$_POST['evaluation'];
+$overview=$_POST['overview'];
 $actions=$_POST['actions'];
+$summary=$_POST['summary'];
 $recommendations=$_POST['recommendations'];
 
-$sql = "INSTERT INTO jobs (case_id, date, owner_first_name, owner_last_name, phone, address, city, state, zip, email, os_make, os_type, os_architecture, sn, ram_amount, cpu_make, cpu_model, cpu_architecture, other_equipment, co, began, previous_work, previous_who, previous_when, comments_initial, boot_time_initial, memory_use_initial, cpu_use_initial, evaluation, overview, actions, summary, recommendations)
-		VALUES ('$case_id', '$date', '$owner_first_name', '$owner_first_name', '$phone', '$email', '$address', '$city', '$state', '$zip', '$email', '$os_make', '$os_type', '$os_architecture', '$sn', '$ram_amount', '$cpu_make', '$cpu_model', '$cpu_architecture', '$other_equipment', '$co', '$began', '$previous_work', '$previous_who', '$previous_when', '$comments_initial', '$boot_time_initial', '$memory_use_initial', '$cpu_use_initial', '$evaluation', '$overview', '$actions', '$summary', '$recommendations')";
+$sql = "INSERT INTO jobs (case_id, day, owner_first_name, owner_last_name, phone, address, city, state, zip, email, os_make, os_type, os_architecture, sn, ram_amount, cpu_make, cpu_model, cpu_architecture, equipment_other, co, began, previous_work, previous_who, previous_when, comments_initial, boot_time_initial, memory_use_initial, cpu_use_initial, evaluation, overview, actions, summary, recommendations) " . " VALUES ('$case_id', '$day', '$owner_first_name', '$owner_first_name', '$phone', '$email', '$address', '$city', '$state', '$zip', '$email', '$os_make', '$os_type', '$os_architecture', '$sn', '$ram_amount', '$cpu_make', '$cpu_model', '$cpu_architecture', '$equipment_other', '$co', '$began', '$previous_work', '$previous_who', '$previous_when', '$comments_initial', '$boot_time_initial', '$memory_use_initial', '$cpu_use_initial', '$evaluation', '$overview', '$actions', '$summary', '$recommendations')";
 
-if (!mysql_query($sql)) {
-	die('Error: ' . mysql_error));
+if(!mysql_query($sql)) {
+	die('Error: ' . mysql_error());
 }
 
 	mysql_close();	
